62517bc359f6fa7d911e1241e7e45aaa84c11ce9
Anna Dang <annadang2001@gmail.com>
2023-07-23 21:28:54 +1000

    Add operate function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,12 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    if (operator == '+') return add(a, b);
+    if (operator == '-') return subtract(a, b);
+    if (operator == 'x') return multiply(a, b);
+    if (operator == '÷') return divide(a, b);
+}
