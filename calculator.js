3c2062271f03a7f17fed5c204baab4f32794e91c
Anna Dang <annadang2001@gmail.com>
2023-07-24 11:50:16 +1000

    Add functions to add digits and operators to screen

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+currentOp = '';
+
 const digitButtons = document.querySelectorAll('.digit');
 const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.getElementById('clear');
@@ -5,6 +7,30 @@ const deleteButton = document.getElementById('delete');
 const percentButton = document.getElementById('percent');
 const periodButton = document.getElementById('period');
 const equalsButton = document.getElementById('equal');
+const displayLastOp = document.getElementById('displayLast');
+const displayCurrentOp = document.getElementById('displayCurrent');
+
+digitButtons.forEach(button => button.addEventListener('click', () => displayOnScreen(button.textContent)));
+operatorButtons.forEach(button => button.addEventListener('click', () => updateOperation(button.textContent)));
+
+function displayOnScreen(digit) {
+    displayCurrentOp.textContent += digit;
+    currentDigit = digit;
+}
+
+function updateOperation(operator) {
+    currentOp = operator
+    displayLastOp.textContent = `${currentDigit} ${currentOp}`;
+}
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    if (operator == '+') return add(a, b);
+    if (operator == '-') return subtract(a, b);
+    if (operator == 'x') return multiply(a, b);
+    if (operator == '÷') return divide(a, b);
+}
 
 function add(a, b) {
     return a + b;
@@ -20,13 +46,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
-}
-
-function operate(operator, a, b) {
-    a = Number(a);
-    b = Number(b);
-    if (operator == '+') return add(a, b);
-    if (operator == '-') return subtract(a, b);
-    if (operator == 'x') return multiply(a, b);
-    if (operator == '÷') return divide(a, b);
 }
